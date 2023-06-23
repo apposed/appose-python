901aecb52ad552a27522a076d4a52fe4e01f4773
Curtis Rueden <ctrueden@wisc.edu>
2023-06-22 21:21:25 -0500

    Improve the test_groovy environment setup
    Until we have a proper environment builder, the bin/test.sh script can
    set up the needed JARs, by pulling down the latest build + dependencies
    from the remote Maven repository.

--- bin/test.sh
+++ bin/test.sh
@@ -10,6 +10,34 @@ set -e
 dir=$(dirname "$0")
 cd "$dir/.."
 
+if [ ! -d target/dependency ]
+then
+  echo "==> Installing appose-java..."
+  mkdir -p target
+  echo '<?xml version="1.0" encoding="UTF-8"?>
+<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
+  <modelVersion>4.0.0</modelVersion>
+  <groupId>org.apposed</groupId>
+  <artifactId>appose-python</artifactId>
+  <version>0-SNAPSHOT</version>
+  <dependencies>
+    <dependency>
+      <groupId>org.apposed</groupId>
+      <artifactId>appose</artifactId>
+      <version>LATEST</version>
+    </dependency>
+  </dependencies>
+  <repositories>
+    <repository>
+      <id>scijava.public</id>
+      <url>https://maven.scijava.org/content/groups/public</url>
+    </repository>
+  </repositories>
+</project>' > appose.pom
+  mvn -f appose.pom dependency:copy-dependencies
+  rm appose.pom
+fi
+
 if [ $# -gt 0 ]
 then
   python -m pytest -p no:faulthandler $@
--- tests/test_appose.py
+++ tests/test_appose.py
@@ -58,15 +58,8 @@ task.outputs["result"] = time
 
 def test_groovy():
     env = appose.system()
-    # FIXME: Resolve these dependencies in a better way.
-    class_path = [
-        "../appose-java/target/appose-0.1.0-SNAPSHOT.jar",
-        "../appose-java/target/dependency/groovy-3.0.4.jar",
-        "../appose-java/target/dependency/groovy-json-3.0.4.jar",
-        "../appose-java/target/dependency/ivy-2.4.0.jar",
-        "../appose-java/target/dependency/jna-5.13.0.jar",
-        "../appose-java/target/dependency/jna-platform-5.13.0.jar",
-    ]
+    # NB: For now, use bin/test.sh to copy the needed JARs.
+    class_path = ["target/dependency/*"]
     with env.groovy(class_path=class_path) as service:
         execute_and_assert(service, collatz_groovy)
 
