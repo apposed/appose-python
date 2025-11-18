d524049cde4dc705bfc7dec406a994e65eae6106
Curtis Rueden <ctrueden@wisc.edu>
2025-11-18 08:53:59 -0600

    CI: cache Maven artifacts between builds

--- bin/test.sh
+++ bin/test.sh
@@ -13,23 +13,7 @@ cd "$dir/.."
 if [ ! -d target/dependency ]
 then
   echo "==> Installing appose-java..."
-  mkdir -p target
-  echo '<?xml version="1.0" encoding="UTF-8"?>
-<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
-  <modelVersion>4.0.0</modelVersion>
-  <groupId>org.apposed</groupId>
-  <artifactId>appose-python</artifactId>
-  <version>0-SNAPSHOT</version>
-  <dependencies>
-    <dependency>
-      <groupId>org.apposed</groupId>
-      <artifactId>appose</artifactId>
-      <version>RELEASE</version>
-    </dependency>
-  </dependencies>
-</project>' > appose.pom
   mvn -f appose.pom dependency:copy-dependencies
-  rm appose.pom
 fi
 
 if [ $# -gt 0 ]
