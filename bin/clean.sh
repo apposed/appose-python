88e01371ac08088dae3c75513a7103f995a7f36c
Curtis Rueden <ctrueden@wisc.edu>
2026-03-07 00:28:09 +0100

    Make clean less ridiculously verbose

--- bin/clean.sh
+++ bin/clean.sh
@@ -3,7 +3,14 @@
 dir=$(dirname "$0")
 cd "$dir/.."
 
+find target/envs -mindepth 1 -maxdepth 1 -type d | while read d
+do
+  echo "$d"
+  rm -rf "$d"
+done
 find . -name __pycache__ -type d | while read d
-  do rm -rfv "$d"
+do
+  echo "$d"
+  rm -rf "$d"
 done
 rm -rfv .pytest_cache build dist src/*.egg-info target
