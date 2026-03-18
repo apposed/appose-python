736ecb54ef727e74dbf30798b7b8a604652e5049
Curtis Rueden <ctrueden@wisc.edu>
2026-03-18 10:23:07 -0500

    Update copyright range bound to 2026

--- src/appose/__init__.py
+++ src/appose/__init__.py
@@ -1,5 +1,5 @@
 # Appose: multi-language interprocess cooperation with shared memory.
-# Copyright (C) 2023 - 2025 Appose developers.
+# Copyright (C) 2023 - 2026 Appose developers.
 # SPDX-License-Identifier: BSD-2-Clause
 
 '''
